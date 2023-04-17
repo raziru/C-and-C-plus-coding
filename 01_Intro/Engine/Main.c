adae7c6315b83397cb031e8bb77947a286f3b4c1
raziru <gudgh2008@daum.net>
2023-04-17 19:14:32 +0900

    dadsddas

--- 01_Intro/Engine/Main.c
+++ 01_Intro/Engine/Main.c
@@ -1,29 +1,10 @@
-#include<stdio.h>//주석
-#include<math.h>
+#include<stdio.h>//아니 헤더파일이 왜사라져
 /*
-* 이거는 긴주석
-* 입니다
-* 정말로
-* [] 대괄호
-* {} 중괄호
-* () 소괄호
+대괄호
+중괄호
+소괄호
 */
-/*
-아니
-진짜네
-*/
-int main() {
-	int a, b, c;
-	a = 10;
-	b = 10;
-	c = 0;
-	for (int i = 0; i < b; i++) {
-		c += a* b;
-	}
-	char* p = "야이 이게 뭐냐\n";
-	printf(p);
-	printf("hello world\n");
-	printf("%d", c);
 
+int main() {
 	return 0;
 }
