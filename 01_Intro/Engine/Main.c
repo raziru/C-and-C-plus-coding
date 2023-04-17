ee386567f9d5e61f09b43aa827926b911c8ef352
raziru <gudgh2008@daum.net>
2023-04-17 19:48:42 +0900

    04-17
    실행할때 비쥬얼 스튜디오 통과해서 실행하기

--- 01_Intro/Engine/Main.c
+++ 01_Intro/Engine/Main.c
@@ -1,10 +1,26 @@
-#include<stdio.h>//아니 헤더파일이 왜사라져
+#include<stdio.h> //standard input output 표준입출력
 /*
-대괄호
-중괄호
-소괄호
-*/
+* 실행할때 비쥬얼 스튜디오를 통해서 실행해봐
+[]:대괄호
+{}:중괄호
+():소괄호
 
-int main() {
+*/
+int main() //진입점(Entry Point)
+{
+	int d;
+	d = 10;
+	printf("Hello World!\n!");
+	printf("할까말까 뭘하나말까\n");
+	printf("세번째 손가락을\n");
+	printf("보기싫어\n");
+	if (d == 10)
+	{
+		printf("d");
+	}
+	else
+	{
+		printf("3");
+	}
 	return 0;
 }
