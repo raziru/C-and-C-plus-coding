9be66074ec9ec15a5c44f5decffe8e71e16e2ec4
raziru <63628772+raziru@users.noreply.github.com>
2023-04-21 22:40:08 +0900

    Add files via upload

--- HomeWork1/Project/Main.cpp
+++ HomeWork1/Project/Main.cpp
@@ -1,13 +1,12 @@
 #include<stdio.h>
 
-int main() 
+int main()
 {
 	int A, B;
 	char p;
 	printf("수식을 입력하세요\n");
-	//scanf_s(" %d %c %d", &A, &p, &B);
 	scanf_s(" %d", &A);
-	scanf_s(" %c", &p);
+	scanf_s(" %c", &p, 1);
 	scanf_s(" %d", &B);
 	switch (p)
 	{
@@ -21,10 +20,24 @@ int main()
 		printf("%d * %d = %d", A, B, A * B);
 		break;
 	case '/':
-		printf("%d / %d = %d", A, B, A / B);
+		if (B == 0)
+		{
+			printf("0으로 나눌 수 없습니다.");
+		}
+		else
+		{
+			printf("%d / %d = %d", A, B, A / B);
+		}
 		break;
 	case '%':
-		printf("%d %% %d = %d", A, B, A % B);
+		if (B == 0)
+		{
+			printf("0으로 나눌 수 없습니다.");
+		}
+		else
+		{
+			printf("%d %% %d = %d", A, B, A % B);
+		}
 		break;
 	default:
 		printf("잘못된 수식입니다.");
